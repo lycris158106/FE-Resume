1282521de19c0f8b386a99cdb1ce66dd46417de5
Lycris <lycris158106@gmail.com>
2017-08-20 09:07:31 +0800

    修改 技能页面 相关显示效果
    修改 技能页面 相关显示效果

--- app/src/js/resume.js
+++ app/src/js/resume.js
@@ -1,8 +1,4 @@
-$(function () {
-  var skills;
-  var content;
-  var $description;
-
+$(document).ready(function () {
   $('#fullpage').fullpage({
     anchors: ['home', 'project', 'skills', 'education', 'career', 'intention', 'download'],
     navigation: true,
@@ -21,21 +17,6 @@ $(function () {
       $(this).find('.slide.project-bootstrap .fp-tableCell').load('../../html/bootstrap.html');
       $(this).find('.slide.project-react .fp-tableCell').load('../../html/react.html');
       $(this).find('.slide.project-bpm .fp-tableCell').load('../../html/bpm.html');
-
-      $.ajax({
-        url: '../../data/skills.json',
-        type: 'GET',
-        success: function (data) {
-          $description = $('.skills').find('.detail-description');
-          content = $description.html();
-          $('.skills').find('.label').hover(function () {
-            skills = Array.prototype.slice.call(this.classList)[2].split('-')[1].toLowerCase();
-            $description.html('<span class="animated fadeIn">' + data[skills] + '</span>');
-          }, function () {
-            $description.html(content);
-          });
-        }
-      });
     }
   });
 });
